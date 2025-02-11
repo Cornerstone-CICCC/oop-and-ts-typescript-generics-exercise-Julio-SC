0aed59d23386711db52d7aad6cc3c1eb72b349c7
Julio-SC <jhver2001@gmail.com>
2025-02-11 11:47:06 -0800

    Exercise Completed

--- dist/ex01.js
+++ dist/ex01.js
@@ -1,9 +1,7 @@
+"use strict";
 // 1. Create a generic arrow function called `getFirstElement` that accepts an array of any type 
 // and returns the first element of the array. 
 // Pass an array of strings ["Hello", "World"] and log the result.
-
-const getFirstElement = <T> (arr: T[]): T => arr[0];
-
+const getFirstElement = (arr) => arr[0];
 console.log(getFirstElement(["Hello", "World"])); // Expected output: "Hello"
-
-console.log(getFirstElement([5,4,3]));
+console.log(getFirstElement([5, 4, 3]));
--- dist/ex02.js
+++ dist/ex02.js
@@ -1,13 +1,7 @@
+"use strict";
 // 2. Create a generic interface called `Data` with two properties: 
 // `id` (number) and `value` (of any type). 
 // Define an object of type `Data` where `value` is a string "Generic" and `id` is 1.
 // Log the object.
-
-interface Data<T> {
-    id: number;
-    value: T;
-  
-}
-
-const data: Data<string> = { id: 1, value: "something" };
+const data = { id: 1, value: "something" };
 console.log(data); // Expected output: { id: 1, value: "Generic" }
